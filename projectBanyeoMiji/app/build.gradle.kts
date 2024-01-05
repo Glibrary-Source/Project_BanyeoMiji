8e4abf60967cecfd76bd5c5f9938ea7703a7958f
Kim <kimgyudo93@gmail.com>
2024-01-05 15:18:33 +0900

    release/1.0.4

--- projectBanyeoMiji/app/build.gradle.kts
+++ projectBanyeoMiji/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.twproject.banyeomiji"
         minSdk = 24
         targetSdk = 33
-        versionCode = 11
+        versionCode = 12
         versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
